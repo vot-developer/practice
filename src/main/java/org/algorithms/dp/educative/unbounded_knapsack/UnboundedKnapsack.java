ceacad5e0efba7270d2ffbe3ce5549bb3ca2e2e4
igor gorchakov <gorchakov.igiv@gmail.com>
2021-01-21 10:36:29 +0300

    solution of 'Unbounded Knapsack'

--- src/main/java/org/algorithms/dp/educative/unbounded_knapsack/UnboundedKnapsack.java
+++ src/main/java/org/algorithms/dp/educative/unbounded_knapsack/UnboundedKnapsack.java
@@ -2,4 +2,65 @@ package org.algorithms.dp.educative.unbounded_knapsack;
 
 public class UnboundedKnapsack {
 
+    //time - O(2^n), space - O(n)
+    public int naive(int[] profits, int[] weights, int capacity) {
+        return doNaive(profits, weights, capacity, 0);
+    }
+
+    private int doNaive(int[] profits, int[] weights, int capacity, int index) {
+        if (capacity == 0 || index == profits.length)
+            return 0;
+
+        int prof1 = 0;
+        if (capacity >= weights[index])
+            prof1 = profits[index] + doNaive(profits, weights, capacity - weights[index], index); //add something
+        int prof2 = doNaive(profits, weights, capacity, index + 1); //add nothing
+
+        return Math.max(prof1, prof2);
+    }
+
+    //time - O(n * capacity), space - O(n * capacity)
+    public int topToDown(int[] profits, int[] weights, int capacity) {
+        return doTopToDown(new Integer[profits.length][capacity + 1], profits, weights, capacity, 0);
+    }
+
+    private int doTopToDown(Integer[][] cache, int[] profits, int[] weights, int capacity, int index) {
+        if (capacity == 0 || index == profits.length)
+            return 0;
+
+        if (cache[index][capacity] != null)
+            return cache[index][capacity];
+
+        int prof1 = 0;
+        if (capacity >= weights[index])
+            prof1 = profits[index] + doNaive(profits, weights, capacity - weights[index], index); //add something
+        int prof2 = doNaive(profits, weights, capacity, index + 1); //add nothing
+
+        cache[index][capacity] = Math.max(prof1, prof2);
+        return cache[index][capacity];
+    }
+
+    //time - O(n * capacity), space - O(n * capacity)
+    public int bottomToUp(int[] profits, int[] weights, int capacity) {
+        int n = profits.length;
+        int[][] aux = new int[n][capacity + 1];
+
+        for (int i = 0; i < n; i++)
+            aux[i][0] = 0;
+
+        for (int s = 1; s <= capacity; s++)
+            aux[0][s] = profits[0] * s;
+
+        int prof1, prof2;
+        for (int i = 1; i < n; i++)
+            for (int s = 1; s <= capacity; s++) {
+                prof1 = aux[i - 1][s]; //add nothing
+                prof2 = 0;
+                if (s >= weights[i])
+                    prof2 = profits[i] + aux[i - 1][s - weights[i]]; //add something
+                aux[i][s] = Math.max(prof1, prof2);
+            }
+
+        return aux[n - 1][capacity];
+    }
 }
--- src/test/java/org/algorithms/dp/educative/unbounded_knapsack/UnboundedKnapsackTest.java
+++ src/test/java/org/algorithms/dp/educative/unbounded_knapsack/UnboundedKnapsackTest.java
@@ -1,12 +1,35 @@
 package org.algorithms.dp.educative.unbounded_knapsack;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UnboundedKnapsackTest {
+    private UnboundedKnapsack unboundedKnapsack;
 
     @BeforeEach
     void setUp() {
+        unboundedKnapsack = new UnboundedKnapsack();
+    }
+
+    @Test
+    void naive() {
+        assertEquals(80, unboundedKnapsack.naive(
+                new int[]{15, 20, 50}, new int[]{1, 2, 3}, 5));
+    }
+
+    @Test
+    void topToDown() {
+        assertEquals(80, unboundedKnapsack.topToDown(
+                new int[]{15, 20, 50}, new int[]{1, 2, 3}, 5));
+    }
+
+    @Test
+    void bottomToUp() {
+        assertEquals(80, unboundedKnapsack.bottomToUp(
+                new int[]{15, 20, 50}, new int[]{1, 2, 3}, 5));
+        assertEquals(140, unboundedKnapsack.bottomToUp(
+                new int[]{15, 50, 60, 90}, new int[]{1, 3, 4, 5}, 8));
     }
 }
