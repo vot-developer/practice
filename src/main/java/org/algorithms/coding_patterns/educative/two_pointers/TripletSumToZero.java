9a348204dddf972f9ad57b460d508cd51e3c673a
igor gorchakov <gorchakov.igiv@gmail.com>
2021-02-02 19:22:41 +0300

    solution of 'Triplet Sum to Zero (medium)'

---
+++ src/main/java/org/algorithms/coding_patterns/educative/two_pointers/TripletSumToZero.java
@@ -0,0 +1,43 @@
+package org.algorithms.coding_patterns.educative.two_pointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
+ */
+public class TripletSumToZero {
+
+    //time - O(n * log n), space - O(1)
+    public static List<List<Integer>> searchTriplets(int[] arr) {
+        if (arr.length < 3) return new ArrayList<>();
+        Arrays.sort(arr);
+        List<List<Integer>> solutions = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0 && arr[i] == arr[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = arr.length - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    solutions.add(Arrays.asList(arr[i], arr[left], arr[right]));
+                    left++;
+                    right--;
+                    while (left < right && arr[left] == arr[left - 1])
+                        left++; // skip same element to avoid duplicate triplets
+                    while (left < right && arr[right] == arr[right + 1])
+                        right--; // skip same element to avoid duplicate triplets
+                }
+            }
+        }
+        return solutions;
+    }
+}
---
+++ src/test/java/org/algorithms/coding_patterns/educative/two_pointers/TripletSumToZeroTest.java
@@ -0,0 +1,28 @@
+package org.algorithms.coding_patterns.educative.two_pointers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TripletSumToZeroTest {
+
+    @Test
+    void searchTriplets1() {
+        List<List<Integer>> result = TripletSumToZero.searchTriplets(new int[]{-3, 0, 1, 2, -1, 1, -2});
+        assertEquals(4, result.size());
+        assertArrayEquals(new Integer[]{-3, 1, 2}, result.get(0).toArray());
+        assertArrayEquals(new Integer[]{-2, 0, 2}, result.get(1).toArray());
+        assertArrayEquals(new Integer[]{-2, 1, 1}, result.get(2).toArray());
+        assertArrayEquals(new Integer[]{-1, 0, 1}, result.get(3).toArray());
+    }
+
+    @Test
+    void searchTriplets2() {
+        List<List<Integer>> result = TripletSumToZero.searchTriplets(new int[]{-3, 0, 1, 2, -1, 1, -2});
+        assertEquals(2, result.size());
+        assertArrayEquals(new Integer[]{-5, 2, 3}, result.get(0).toArray());
+        assertArrayEquals(new Integer[]{-2, -1, 3}, result.get(1).toArray());
+    }
+}
