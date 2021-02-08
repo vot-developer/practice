0994b2b823c9726a62d8281ec3aa474113c61111
igor gorchakov <gorchakov.igiv@gmail.com>
2021-02-08 10:55:49 +0300

    solution of 'Conflicting Appointments (medium)'

---
+++ src/main/java/org/algorithms/coding_patterns/educative/merge_intervals/ConflictingAppointments.java
@@ -0,0 +1,22 @@
+package org.algorithms.coding_patterns.educative.merge_intervals;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+ */
+public class ConflictingAppointments {
+    //time - O(n log n), space - O(1)
+    public static boolean canAttendAllAppointments(Interval[] intervals) {
+        if (intervals.length < 2)
+            return true;
+
+        Arrays.sort(intervals, Comparator.comparingInt(value -> value.start));
+        for (int i = 1; i < intervals.length; i++)
+            if (intervals[i - 1].end > intervals[i].start)
+                return false;
+
+        return true;
+    }
+}
