db4b18d0cfd6664b953a45ba1618eda2839be18c
igor gorchakov <gorchakov.igiv@gmail.com>
2021-02-08 21:36:09 +0300

    fix

--- src/test/java/org/algorithms/coding_patterns/educative/fast_slow_pointers/MiddleOfLinkedListTest.java
+++ src/test/java/org/algorithms/coding_patterns/educative/fast_slow_pointers/MiddleOfLinkedListTest.java
@@ -10,7 +10,7 @@ class MiddleOfLinkedListTest {
 
     @BeforeEach
     void setUp() {
-        ListNode head = new ListNode(1);
+        head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
@@ -19,11 +19,11 @@ class MiddleOfLinkedListTest {
 
     @Test
     void findMiddle() {
-        assertEquals(3, MiddleOfLinkedList.findMiddle(head));
+        assertEquals(3, MiddleOfLinkedList.findMiddle(head).value);
         head.next.next.next.next.next = new ListNode(6);
-        assertEquals(4, MiddleOfLinkedList.findMiddle(head));
+        assertEquals(4, MiddleOfLinkedList.findMiddle(head).value);
         head.next.next.next.next.next.next = new ListNode(7);
-        assertEquals(4, MiddleOfLinkedList.findMiddle(head));
+        assertEquals(4, MiddleOfLinkedList.findMiddle(head).value);
     }
 
 
