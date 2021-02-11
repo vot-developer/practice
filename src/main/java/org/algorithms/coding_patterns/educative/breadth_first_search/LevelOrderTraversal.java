8db73529c29bf240889f6e3a4756ae26a8cbf3cb
igor gorchakov <gorchakov.igiv@gmail.com>
2021-02-11 13:39:07 +0300

    solution of 'Binary Tree Level Order Traversal (easy)'

--- src/main/java/org/algorithms/coding_patterns/educative/breadth_first_search/LevelOrderTraversal.java
+++ src/main/java/org/algorithms/coding_patterns/educative/breadth_first_search/LevelOrderTraversal.java
@@ -1,8 +1,37 @@
 package org.algorithms.coding_patterns.educative.breadth_first_search;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /*
 Given a binary tree, populate an array to represent its level-by-level traversal.
 You should populate the values of all nodes of each level from left to right in separate sub-arrays.
  */
 public class LevelOrderTraversal {
+
+    //time - O(n), space - O(n)
+    public static List<List<Integer>> traverse(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>(size);
+            for (int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
 }
--- src/test/java/org/algorithms/coding_patterns/educative/breadth_first_search/LevelOrderTraversalTest.java
+++ src/test/java/org/algorithms/coding_patterns/educative/breadth_first_search/LevelOrderTraversalTest.java
@@ -1,7 +1,26 @@
 package org.algorithms.coding_patterns.educative.breadth_first_search;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LevelOrderTraversalTest {
 
+    @Test
+    void test1() {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        List<List<Integer>> result = LevelOrderTraversal.traverse(root);
+        assertEquals(3, result.size());
+        assertArrayEquals(new Integer[]{12}, result.get(0).toArray());
+        assertArrayEquals(new Integer[]{7, 1}, result.get(1).toArray());
+        assertArrayEquals(new Integer[]{9, 10, 5}, result.get(2).toArray());
+    }
 }
