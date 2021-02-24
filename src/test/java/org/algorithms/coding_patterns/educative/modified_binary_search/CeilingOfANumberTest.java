6186e0b59a36165a8d2d357a164a859b0a628958
igor gorchakov <gorchakov.igiv@gmail.com>
2021-02-24 16:29:23 +0300

    solution of 'Ceiling of a Number (medium)'

---
+++ src/test/java/org/algorithms/coding_patterns/educative/modified_binary_search/CeilingOfANumberTest.java
@@ -0,0 +1,16 @@
+package org.algorithms.coding_patterns.educative.modified_binary_search;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CeilingOfANumberTest {
+
+    @Test
+    void searchCeilingOfANumber() {
+        assertEquals(1, CeilingOfANumber.searchCeilingOfANumber(new int[] { 4, 6, 10 }, 6));
+        assertEquals(4, CeilingOfANumber.searchCeilingOfANumber(new int[] { 1, 3, 8, 10, 15 }, 12));
+        assertEquals(-1, CeilingOfANumber.searchCeilingOfANumber(new int[] { 4, 6, 10 }, 17));
+        assertEquals(0, CeilingOfANumber.searchCeilingOfANumber(new int[] { 4, 6, 10 }, -1));
+    }
+}
