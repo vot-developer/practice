40a1b38fae0f1470c8f7c0df8591eb9dbb151235
igor gorchakov <gorchakov.igiv@gmail.com>
2021-03-13 20:06:35 +0300

    Topological Sort (medium) ver 2

--- src/main/java/org/algorithms/coding_patterns/educative/topological_sort/TopologicalSort.java
+++ src/main/java/org/algorithms/coding_patterns/educative/topological_sort/TopologicalSort.java
@@ -1,9 +1,13 @@
 package org.algorithms.coding_patterns.educative.topological_sort;
 
 import java.util.ArrayList;
-import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 /*
 Given a directed graph, find the topological ordering of its vertices.
@@ -11,31 +15,43 @@ Given a directed graph, find the topological ordering of its vertices.
 public class TopologicalSort {
     //time - O(E+V), space - O(V)
     public static List<Integer> sort(int vertices, int[][] edges) {
-        Deque<Integer> stack = new LinkedList<>();
-        boolean[] marked = new boolean[vertices];
+        List<Integer> sortedOrder = new ArrayList<>();
+        if (vertices <= 0)
+            return sortedOrder;
 
-        for (int v = 0; v < vertices; v++)
-            dfs(v, edges, marked, stack);
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        Map<Integer, Integer> inDegree = new HashMap<>(vertices);
+        for (int i = 0; i < vertices; i++) {
+            graph.put(i, new HashSet<>());
+            inDegree.put(i, 0);
+        }
 
-        return new ArrayList<>(stack);
-    }
+        for (int[] vs : edges) {
+            int v = vs[0];
+            int w = vs[1];
+            inDegree.put(w, inDegree.get(w) + 1);
+            graph.get(v).add(w);
+            graph.get(w).add(v);
+        }
 
-    private static void dfs(int v, int[][] edges, boolean[] marked, Deque<Integer> stack) {
-        if (marked[v])
-            return;
+        Queue<Integer> sources = new LinkedList<>();
+        for (int i = 0; i < vertices; i++)
+            if (inDegree.get(i) == 0)
+                sources.offer(i);
 
-        marked[v] = true;
-        for (int w : getAdj(edges, v))
-            dfs(w, edges, marked, stack);
-        stack.addFirst(v);
-    }
+        while (!sources.isEmpty()) {
+            Integer v = sources.poll();
+            sortedOrder.add(v);
+            for (int w : graph.get(v)) {
+                inDegree.put(w, inDegree.get(w) - 1);
+                if (inDegree.get(w) == 0)
+                    sources.offer(w);
+            }
+        }
 
-    private static List<Integer> getAdj(int[][] edges, int v) {
-        List<Integer> result = new ArrayList<>();
-        for (int[] verticies : edges)
-            if (verticies[0] == v)
-                result.add(verticies[1]);
+        if (sortedOrder.size() != vertices) // topological sort is not possible as the graph has a cycle
+            return new ArrayList<>();
 
-            return result;
+        return sortedOrder;
     }
 }
--- src/test/java/org/algorithms/coding_patterns/educative/topological_sort/TopologicalSortTest.java
+++ src/test/java/org/algorithms/coding_patterns/educative/topological_sort/TopologicalSortTest.java
@@ -12,7 +12,7 @@ class TopologicalSortTest {
     void sort1() {
         List<Integer> result = TopologicalSort.sort(4,
             new int[][] { new int[] { 3, 2 }, new int[] { 3, 0 }, new int[] { 2, 0 }, new int[] { 2, 1 } });
-        assertArrayEquals(new Integer[]{3, 2, 1, 0}, result.toArray());
+        assertArrayEquals(new Integer[]{3, 2, 0, 1}, result.toArray());
     }
 
     @Test
@@ -20,13 +20,13 @@ class TopologicalSortTest {
         List<Integer> result = TopologicalSort.sort(5, new int[][]
             { new int[] { 4, 2 }, new int[] { 4, 3 }, new int[] { 2, 0 },
             new int[] { 2, 1 }, new int[] { 3, 1 } });
-        assertArrayEquals(new Integer[]{4, 3, 2, 1, 0}, result.toArray());
+        assertArrayEquals(new Integer[]{4, 2, 3, 0, 1}, result.toArray());
     }
 
     @Test
     void sort3() {
         List<Integer> result = TopologicalSort.sort(7, new int[][] { new int[] { 6, 4 }, new int[] { 6, 2 }, new int[] { 5, 3 },
             new int[] { 5, 4 }, new int[] { 3, 0 }, new int[] { 3, 1 }, new int[] { 3, 2 }, new int[] { 4, 1 } });
-        assertArrayEquals(new Integer[]{6, 5, 4, 3, 2, 1, 0}, result.toArray());
+        assertArrayEquals(new Integer[]{5, 6, 3, 4, 0, 2, 1}, result.toArray());
     }
 }
