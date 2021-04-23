ea138941396a7f505741689bca753c3494163610
igor gorchakov <gorchakov.igiv@gmail.com>
2021-04-23 20:16:20 +0300

    fix

--- src/test/java/org/algorithms/coding_patterns/educative/modified_binary_search/MinimumDifferenceTest.java
+++ src/test/java/org/algorithms/coding_patterns/educative/modified_binary_search/MinimumDifferenceTest.java
@@ -8,9 +8,10 @@ class MinimumDifferenceTest {
 
     @Test
     void searchMinDiffElement() {
-//        assertEquals(6, MinimumDifference.searchMinDiffElement(new int[] { 4, 6, 10 }, 7));
-//        assertEquals(4, MinimumDifference.searchMinDiffElement(new int[] { 4, 6, 10 }, 4));
-//        assertEquals(10, MinimumDifference.searchMinDiffElement(new int[] { 1, 3, 8, 10, 15 }, 12));
+        assertEquals(6, MinimumDifference.searchMinDiffElement(new int[] { 4, 6, 10 }, 7));
+        assertEquals(4, MinimumDifference.searchMinDiffElement(new int[] { 4, 6, 10 }, 4));
+        assertEquals(10, MinimumDifference.searchMinDiffElement(new int[] { 1, 3, 8, 10, 15 }, 12));
         assertEquals(10, MinimumDifference.searchMinDiffElement(new int[] { 4, 6, 10 }, 17));
+        assertEquals(7, MinimumDifference.searchMinDiffElement(new int[] { 2, 4, 7, 9 }, 6));
     }
 }
