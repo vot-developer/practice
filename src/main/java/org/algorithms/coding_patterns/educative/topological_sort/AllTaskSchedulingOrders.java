d6895fdbd4492e9aca6ef19b37b658389980c629
igor gorchakov <gorchakov.igiv@gmail.com>
2022-01-12 11:05:49 +0300

    fix

--- src/main/java/org/algorithms/coding_patterns/educative/topological_sort/AllTaskSchedulingOrders.java
+++ src/main/java/org/algorithms/coding_patterns/educative/topological_sort/AllTaskSchedulingOrders.java
@@ -2,12 +2,10 @@ package org.algorithms.coding_patterns.educative.topological_sort;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 
 /*
 There are 'N' tasks, labeled from '0' to 'N-1'. Each task can have some prerequisite tasks which need to be completed 
@@ -17,79 +15,69 @@ possible ordering of tasks meeting all prerequisites.
 public class AllTaskSchedulingOrders {
 
     public static List<List<Integer>> printOrders(int tasks, int[][] prerequisites) {
-        Queue<List<Integer>> sortedOrders = new LinkedList<>();
-        if (tasks <= 0)
-            return new ArrayList<>(sortedOrders);
-        sortedOrders.offer(new ArrayList<>());
 
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
-        Map<Integer, Integer> inDegree = new HashMap<>(tasks);
+        // a. Initialize the graph
+        HashMap<Integer, Integer> inDegree = new HashMap<>(); // count of incoming edges for every vertex
+        HashMap<Integer, List<Integer>> graph = new HashMap<>(); // adjacency list graph
         for (int i = 0; i < tasks; i++) {
-            graph.put(i, new HashSet<>());
             inDegree.put(i, 0);
+            graph.put(i, new ArrayList<Integer>());
         }
 
-        for (int[] vs : prerequisites) {
-            int v = vs[0];
-            int w = vs[1];
-            inDegree.put(w, inDegree.get(w) + 1);
-            graph.get(v).add(w);
-            graph.get(w).add(v);
+        // b. Build the graph
+        for (int i = 0; i < prerequisites.length; i++) {
+            int parent = prerequisites[i][0], child = prerequisites[i][1];
+            graph.get(parent).add(child); // put the child into it's parent's list
+            inDegree.put(child, inDegree.get(child) + 1); // increment child's inDegree
         }
 
+        // c. Find all sources i.e., all vertices with 0 in-degrees
         Queue<Integer> sources = new LinkedList<>();
-        for (int i = 0; i < tasks; i++)
-            if (inDegree.get(i) == 0)
-                sources.offer(i);
-
-        while (!sources.isEmpty()) {
-            int size = sources.size();
-            List<Integer> order = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                Integer v = sources.poll();
-                order.add(v);
-                for (int w : graph.get(v)) {
-                    inDegree.put(w, inDegree.get(w) - 1);
-                    if (inDegree.get(w) == 0)
-                        sources.offer(w);
-                }
-            }
-
-            List<List<Integer>> permute = permute(order);
-            int resultSize = sortedOrders.size();
-            for (int r = 0; r < resultSize; r++) {
-                List<Integer> sortedOrder = sortedOrders.poll();
-                for (int i = 0; i < permute.size(); i++) {
-                    List<Integer> result = new ArrayList<>(sortedOrder);
-                    result.addAll(permute.get(i));
-                    sortedOrders.offer(result);
-                }
-            }
+        for (Map.Entry<Integer, Integer> entry : inDegree.entrySet()) {
+            if (entry.getValue() == 0)
+                sources.add(entry.getKey());
         }
 
-        return new ArrayList<>(sortedOrders);
+        List<List<Integer>> ans = new ArrayList<>();
+        createAllTopologicalSorts(tasks, graph, inDegree, sources, new ArrayList<>(), ans);
+        return ans;
     }
 
-    private static List<List<Integer>> permute(List<Integer> nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Queue<List<Integer>> queue = new LinkedList<>();
-        queue.add(new ArrayList<>());
-        for (int num : nums) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                List<Integer> set = queue.poll();
-                int setSize = set.size();
-                for (int j = 0; j <= setSize; j++) {
-                    List<Integer> perm = new ArrayList<>(set);
-                    perm.add(j, num);
-                    if (perm.size() == nums.size())
-                        result.add(perm);
-                    else
-                        queue.offer(perm);
+    private static void createAllTopologicalSorts(int tasks, HashMap<Integer, List<Integer>> graph, HashMap<Integer, Integer> inDegree,
+                                                  Queue<Integer> sources, List<Integer> sortedOrder, List<List<Integer>> ans) {
+        if (sortedOrder.size() == tasks)
+            ans.add(new ArrayList<>(sortedOrder));
+
+        if (!sources.isEmpty()) {
+            for (Integer vertex : sources) {
+                sortedOrder.add(vertex);
+                Queue<Integer> sourcesForNextCall = cloneQueue(sources);
+                // only remove the current source, all other sources should remain in the queue for the next call
+                sourcesForNextCall.remove(vertex);
+                List<Integer> children = graph.get(vertex); // get the node's children to decrement their in-degrees
+                for (int child : children) {
+                    inDegree.put(child, inDegree.get(child) - 1);
+                    if (inDegree.get(child) == 0)
+                        sourcesForNextCall.add(child); // save the new source for the next call
                 }
+
+                // recursive call to print other orderings from the remaining (and new) sources
+                createAllTopologicalSorts(tasks, graph, inDegree, sourcesForNextCall, sortedOrder, ans);
+
+                // backtrack, remove the vertex from the sorted order and put all of its children back to consider
+                // the next source instead of the current vertex
+                sortedOrder.remove(vertex);
+                for (int child : children)
+                    inDegree.put(child, inDegree.get(child) + 1);
             }
         }
-        return result;
+    }
+
+    // makes a deep copy of the queue
+    private static Queue<Integer> cloneQueue(Queue<Integer> queue) {
+        Queue<Integer> clone = new LinkedList<>();
+        for (Integer num : queue)
+            clone.add(num);
+        return clone;
     }
 }
--- src/test/java/org/algorithms/coding_patterns/educative/topological_sort/AllTaskSchedulingOrdersTest.java
+++ src/test/java/org/algorithms/coding_patterns/educative/topological_sort/AllTaskSchedulingOrdersTest.java
@@ -10,9 +10,11 @@ class AllTaskSchedulingOrdersTest {
 
     @Test
     void printOrders() {
-        List<List<Integer>> result = AllTaskSchedulingOrders.printOrders(4,
-            new int[][] { new int[] { 3, 2 }, new int[] { 3, 0 }, new int[] { 2, 0 }, new int[] { 2, 1 } });
-        assertArrayEquals(new Integer[]{3, 2, 1, 0}, result.get(0).toArray());
-        assertArrayEquals(new Integer[]{3, 2, 0, 1}, result.get(1).toArray());
+        List<List<Integer>> result = AllTaskSchedulingOrders.printOrders(6,
+            new int[][] { new int[] { 2, 5 }, new int[] { 0, 5 }, new int[] { 0, 4 }, new int[] { 1, 4 },
+                new int[]{3, 2}, new int[]{1, 3}
+        });
+        assertArrayEquals(new Integer[]{0, 1, 4, 3, 2, 5}, result.get(0).toArray());
+        assertArrayEquals(new Integer[]{1, 3, 2, 0, 4, 5}, result.get(12).toArray());
     }
 }
